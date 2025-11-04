865d28d37c2425040200c69fbdf92c3d5ff3c6f9
karan fartyal <karanfartyaloffice@gmail.com>
2025-11-04 22:39:38 +0530

    change title

--- app/companion/new/page.tsx
+++ app/companion/new/page.tsx
@@ -2,16 +2,15 @@ import CompanionForm from "@/components/CompanionForm";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
 const page = async () => {
   const { userId } = await auth();
-  
+
   if (!userId) {
     redirect("/sign-in");
   }
   return (
     <div className="w-full h-[80vh] flex flex-col  justify-center items-center">
-      <div className="w-1/3">
+      <div className="w-1/3 max-lg:w-full">
         <CompanionForm />
       </div>
     </div>
